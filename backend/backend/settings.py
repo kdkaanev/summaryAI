28d8904da15174f178780cec34a5917f6259a118
kdkaanev <kdkaane@github.io>
2025-08-18 19:06:00 +0300

    add secret

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET")
+DJANGO_SECRET_KEY= os.getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "Тrue")== "True"
